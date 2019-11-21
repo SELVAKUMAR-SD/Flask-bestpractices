eb246afac65058e527988e50b395f39ade907478
divum-selvakumar <senapati.krishna@divum.in>
2019-11-21 16:23:56 +0530

    nothing

--- api/auth.py
+++ api/auth.py
@@ -15,7 +15,7 @@ def decode_auth_token(auth_token):
     """
     Decodes the auth token
     :param auth_token:
-    :return: integer|error respons
+    :return: integer|error response
     """
     if not auth_token:
         raise UnauthorizedError(strings.TOKEN_MISSING)
